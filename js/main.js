38db3c4276373da156b846072edfe8534c988bd6
Artem Lipchansky <lipchanskiy.an@gmail.com>
2023-06-25 22:30:56 +0300

    added

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// const myName = "Artem";
+
+// console.log("Hello, world!");
+// console.log(myName);
+// // console.log("Artem");
+// console.log(30 + 39);
+// console.log("Good morning");
+
+
+
+const title = document.querySelector('.cats-title')
+console.log(title);
+
+title.style.cssText = "color: green; font-size: 70px";
+title.style.fontSize = "80px";
+
+
+// console.dir(document);
+
+
+
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+// Object
+
+
+
